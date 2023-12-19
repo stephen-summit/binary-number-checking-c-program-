8c77df8977a303aeec6df37b355bf0f2163f6e5c
Sumit <135416708+stephen-summit@users.noreply.github.com>
2023-12-19 23:42:07 +0530

    Add files via upload

---
+++ check_binary.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class binary
+{
+private:
+    string s;
+
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones_Complement(void);
+    void display(void);
+};
+
+void binary :: read(void)
+{
+    cout << "Enter a binary number" << endl;
+    cin >> s;
+}
+
+void binary::chk_bin(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout << "Incorrect binary format " << endl;
+            exit(0);
+        }
+    }
+}
+
+void binary :: ones_Complement(void)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0')
+        {
+            s.at(i) = '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+void binary :: display(void)
+{
+    cout << "Displaying your binary number" << endl;
+    for (int i = 0; i < s.length(); i++)
+    {
+        cout << s.at(i);
+    }
+    cout<<endl;
+}
+int main()
+{
+    binary b;
+    b.read();
+    b.chk_bin();
+    b.display();
+    b.ones_Complement();
+    b.display();
+    return 0;
+}
